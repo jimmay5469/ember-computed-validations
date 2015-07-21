74e78bf074a77a54f7c98d0cd74edaa57eeb55d6
jimmay5469 <jimmay5469@gmail.com>
2015-07-20 23:35:38 -0400

    Add tests

--- tests/unit/mixins/computed-validations-test.js
+++ tests/unit/mixins/computed-validations-test.js
@@ -10,3 +10,104 @@ test('it works', function(assert) {
   var subject = ComputedValidationsObject.create();
   assert.ok(subject);
 });
+
+test('it can validate with a computed property', function(assert) {
+  var ComputedValidationsObject = Ember.Object.extend(ComputedValidationsMixin, {
+    firstName: null,
+    firstNameNotEmpty: Ember.computed.notEmpty('firstName'),
+    computedValidations: {
+      firstName: {
+        firstNameNotEmpty: 'First name is a required field.'
+      }
+    }
+  });
+  var subject = ComputedValidationsObject.create();
+  assert.equal(subject.get('computedIsValid'), false);
+  assert.equal(subject.get('computedIsInvalid'), true);
+  assert.deepEqual(subject.get('computedErrors.firstName'), ['First name is a required field.']);
+
+  subject.set('firstName', 'Bob');
+
+  assert.equal(subject.get('computedIsValid'), true);
+  assert.equal(subject.get('computedIsInvalid'), false);
+  assert.deepEqual(subject.get('computedErrors.firstName'), []);
+});
+
+test('it can validate without a computed property', function(assert) {
+  var ComputedValidationsObject = Ember.Object.extend(ComputedValidationsMixin, {
+    firstName: null,
+    computedValidations: {
+      firstName: {
+        firstName: 'First name is a required field.'
+      }
+    }
+  });
+  var subject = ComputedValidationsObject.create();
+  assert.equal(subject.get('computedIsValid'), false);
+  assert.equal(subject.get('computedIsInvalid'), true);
+  assert.deepEqual(subject.get('computedErrors.firstName'), ['First name is a required field.']);
+
+  subject.set('firstName', 'Bob');
+
+  assert.equal(subject.get('computedIsValid'), true);
+  assert.equal(subject.get('computedIsInvalid'), false);
+  assert.deepEqual(subject.get('computedErrors.firstName'), []);
+});
+
+test('it can validate a nested property', function(assert) {
+  var ComputedValidationsObject = Ember.Object.extend(ComputedValidationsMixin, {
+    user: null,
+    computedValidations: {
+      firstName: {
+        'user.firstName': 'First name is a required field.'
+      }
+    }
+  });
+  var subject = ComputedValidationsObject.create();
+  assert.equal(subject.get('computedIsValid'), false);
+  assert.equal(subject.get('computedIsInvalid'), true);
+  assert.deepEqual(subject.get('computedErrors.firstName'), ['First name is a required field.']);
+
+  subject.set('user', {firstName:'Bob'});
+
+  assert.equal(subject.get('computedIsValid'), true);
+  assert.equal(subject.get('computedIsInvalid'), false);
+  assert.deepEqual(subject.get('computedErrors.firstName'), []);
+});
+
+test('it can validate a property with multiple validations', function(assert) {
+  var ComputedValidationsObject = Ember.Object.extend(ComputedValidationsMixin, {
+    user: null,
+    passwordsMatch: Ember.computed('user.password1', 'user.password2', function() {
+      return this.get('user.password1') === this.get('user.password2');
+    }),
+    computedValidations: {
+      password: {
+        'user.password1': 'Please enter a password.',
+        passwordsMatch: 'The password fields must match.'
+      }
+    }
+  });
+  var subject = ComputedValidationsObject.create();
+  assert.equal(subject.get('computedIsValid'), false);
+  assert.equal(subject.get('computedIsInvalid'), true);
+  assert.deepEqual(subject.get('computedErrors.password'), ['Please enter a password.']);
+
+  subject.set('user', {password1: 'p@ssw0rd!'});
+
+  assert.equal(subject.get('computedIsValid'), false);
+  assert.equal(subject.get('computedIsInvalid'), true);
+  assert.deepEqual(subject.get('computedErrors.password'), ['The password fields must match.']);
+
+  subject.set('user.password2', 'p@ssw0rd!');
+
+  assert.equal(subject.get('computedIsValid'), true);
+  assert.equal(subject.get('computedIsInvalid'), false);
+  assert.deepEqual(subject.get('computedErrors.password'), []);
+
+  subject.set('user.password1', null);
+
+  assert.equal(subject.get('computedIsValid'), false);
+  assert.equal(subject.get('computedIsInvalid'), true);
+  assert.deepEqual(subject.get('computedErrors.password'), ['Please enter a password.', 'The password fields must match.']);
+});
